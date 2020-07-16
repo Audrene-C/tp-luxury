d8ae794efda49a6bde9793164d3c70652eba0395
azerty <matthias.pellacoeur@hotmail.com>
2020-07-16 10:15:16 +0200

    debbug

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Candidate;
+use App\Form\CandidateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CandidateController extends AbstractController
 {
+    // /**
+    //  * @Route("/candidate", name="candidate")
+    //  */
+    // public function index()
+    // {
+    //     return $this->render('candidate/index.html.twig', [
+    //         'controller_name' => 'CandidateController',
+    //     ]);
+    // }
+
     /**
-     * @Route("/candidate", name="candidate")
+     * @Route("/candidate/{id}", name="candidate", methods={"GET", "POST"})
      */
-    public function index()
+    public function account(Request $request): Response
     {
-        return $this->render('candidate/index.html.twig', [
-            'controller_name' => 'CandidateController',
-        ]);
+        $candidate = new Candidate();
+        $form = $this->createForm(CandidateType::class, $candidate);
+        $form->handleRequest($request);
     }
 }
